3312e7cfee7ddbc9e7db87d446bb3cf4590ee526
pierrek6e <pierrecass75@gmail.com>
2022-04-25 10:43:00 +0700

    bug fix

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const video = document.getElementById("video");
 const range = document.getElementById("range");
 const playBtn = document.getElementById("play-btn");
+const playBtnIco = document.getElementById("play-btn-ico");
 const stopBtn = document.getElementById("stop-btn");
 let isVideoPlaying = false;
 let timer = document.getElementById("timer");
@@ -10,19 +11,25 @@ initPlayer();
 playBtn.addEventListener("click", () => {
   if (isVideoPlaying) {
     isVideoPlaying = false;
-    video.play();
+    video.pause();
     toggleButton();
     updateRange();
   } else {
     isVideoPlaying = true;
-    video.pause();
+    video.play();
     toggleButton();
     updateRange();
   }
 });
 
 stopBtn.addEventListener("click", () => {
+  range.value = 0;
+  video.currentTime = range.value;
+  updateTimer();
   video.pause();
+  isVideoPlaying = false;
+  playBtnIco.innerText = "play_circle";
+  playBtnIco.className = "material-icons";
 });
 
 function updateRange() {
@@ -40,14 +47,30 @@ function initPlayer() {
 }
 
 function toggleButton() {
-  playBtn.className = isVideoPlaying ? "chair" : "pause_circle";
+  if (isVideoPlaying) {
+    playBtnIco.innerText = "pause_circle";
+  } else {
+    playBtnIco.innerText = "play_circle";
+  }
 }
 
 function updateTimer() {
-  timer.innerText = range.value;
+  timer.innerText = convertHMS(range.value);
 }
 
 range.addEventListener("change", () => {
   video.currentTime = range.value;
   updateTimer();
 });
+
+function convertHMS(value) {
+  const sec = parseInt(value, 10); // convert value to number if it's string
+  let hours   = Math.floor(sec / 3600); // get hours
+  let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+  let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+  // add 0 if value < 10; Example: 2 => 02
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return hours+':'+minutes+':'+seconds; // Return is HH : MM : SS
+}
